52f8829d364aaf91050b819f44997f86ecf79312
KMarsh28 <64782267+KMarsh28@users.noreply.github.com>
2020-09-08 14:42:36 +0100

    08/09/2020

--- Assets/Scripts/AddPoints.cs
+++ Assets/Scripts/AddPoints.cs
@@ -5,12 +5,24 @@ using TMPro;
 
 public class AddPoints : MonoBehaviour {
 
-	private int score;
+	public int score;
 	public TextMeshPro scoreText;
+	private GameObject[] collect5ptArray;
+	private int count5pt;
+	public TextMeshPro goodText;
+	private GameObject[] minus10ptArray;
+	private int minus10pt;
+	public TextMeshPro badText;
 
 	void Start ()
 	{
 		score = 0;
+		collect5ptArray = GameObject.FindGameObjectsWithTag ("collectible5pt");
+		count5pt = collect5ptArray.Length;
+		goodText.text = "Good: " + count5pt.ToString ();
+		minus10ptArray = GameObject.FindGameObjectsWithTag ("minus10pt");
+		minus10pt = minus10ptArray.Length;
+		badText.text = "Bad: " + minus10pt.ToString ();
 	}
 
 	void OnCollisionEnter(Collision col)
@@ -21,6 +33,8 @@ public class AddPoints : MonoBehaviour {
 			Destroy (col.gameObject);
 			score -= 10;
 			scoreText.text = "Score: " + score.ToString ();
+			minus10pt -= 1;
+			badText.text = "Bad: " + minus10pt.ToString ();
 		}
 	}
 
@@ -32,6 +46,8 @@ public class AddPoints : MonoBehaviour {
 			Destroy (col.gameObject);
 			score += 5;
 			scoreText.text = "Score: " + score.ToString ();
+			count5pt -= 1;
+			goodText.text = "Good: " + count5pt.ToString ();
 		}
 	}
 }
---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckWin : MonoBehaviour {
+
+	private int gameScore;
+	public TextMeshPro timerText;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timerText.text = "Time: " + Time.timeSinceLevelLoad.ToString ();	
+		/*Finds the object tagged as player, then finds the script attached called AddPoints, then pulls out the score value*/
+		gameScore = GameObject.FindWithTag ("player").GetComponent<AddPoints> ().score;
+		//Debug.Log ("Game score = " + gameScore);
+		if (gameScore >= 10) 
+		{
+			Debug.Log ("WINNER!");
+		} 
+		else if (gameScore < 0) 
+		{
+			Debug.Log ("LOSER!");
+		}
+	}
+}
