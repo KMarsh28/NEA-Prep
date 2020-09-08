1898708ae951b1a7b91bdea2db6c69a173a00ee8
KMarsh28 <64782267+KMarsh28@users.noreply.github.com>
2020-09-08 12:16:48 +0100

    End of Task 3
    Didn't submit the first two because I'm inept :)

---
+++ Assets/Scripts/AddPoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPoints : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "collectible5pt")
+		{
+			Debug.Log ("5 points were added");
+			Destroy (col.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.freezeRotation = true;
+	}
+	//Calls the physics object and stops it from rotating
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "rightWall")
+		{
+			//Debug.Log("I collided with the " + col.gameObject.tag);	
+		}
+		else if (col.gameObject.tag == "leftWall")
+		{
+			//Debug.Log("I collided with the " + col.gameObject.tag);
+		}
+		else if (col.gameObject.tag == "floor")
+		{
+			//Debug.Log("I collided with the " + col.gameObject.tag);
+		}
+	}
+	//Logs whenever the physics object collides with something
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			rb.AddForce (new Vector3 (1, 0, 0), ForceMode.Impulse);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			rb.AddForce (new Vector3 (-1, 0, 0), ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rb.AddForce (new Vector3 (0, 5, 0), ForceMode.Impulse);
+			//Debug.Log ("Jump Jump!");
+		}
+	}
+	//Used to control the physics object
+}
