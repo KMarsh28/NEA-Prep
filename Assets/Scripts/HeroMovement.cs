037ecc531e9e07d58f616064a9a88b4ff68a7cfd
KMarsh28 <64782267+KMarsh28@users.noreply.github.com>
2020-09-15 14:39:53 +0100

    End of Task 10
    Co-Authored-By: Andy O'Hara <37961382+compohara@users.noreply.github.com>

---
+++ Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 3.1f;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			rb.AddForce(new Vector2(-1, 0), ForceMode2D.Impulse);
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+		}
+	}
+}
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+	Transform heroTR;
+
+	// Use this for initialization
+	void Start () 
+	{
+		heroTR = GameObject.FindGameObjectWithTag ("hero").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		//Debug.Log ("SPIKED!");
+		heroTR.SetPositionAndRotation(new Vector3(-5.5f, 1.265f, 0), Quaternion.identity);
+	}
+}
