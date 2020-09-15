4bca49bbc1bc38b3590dac5c8ab01c671885c395
KMarsh28 <64782267+KMarsh28@users.noreply.github.com>
2020-09-15 13:23:05 +0100

    End of Task 9
    Co-Authored-By: Andy O'Hara <37961382+compohara@users.noreply.github.com>

---
+++ Assets/Scripts/DinoAnimation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimation : MonoBehaviour {
+
+	Animator myAnimator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			myAnimator.SetBool ("walk", true);
+		} 
+		else 
+		{
+			myAnimator.SetBool ("walk", false);
+		}
+
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			myAnimator.SetBool ("jump", true);
+		} 
+		else 
+		{
+			myAnimator.SetBool ("jump", false);
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			myAnimator.SetBool ("dead", true);
+		} 
+		else if (Input.GetKey (KeyCode.Q) && myAnimator.GetBool ("dead") == true) 
+		{
+			Debug.Log ("spawn");
+			myAnimator.Play ("Respawn");
+			myAnimator.SetBool ("dead", false);
+		}
+
+		if (Input.GetKey (KeyCode.R)) 
+		{
+			myAnimator.SetBool ("run", true);
+		} 
+		else 
+		{
+			myAnimator.SetBool ("run", false);
+		}
+	}
+}
